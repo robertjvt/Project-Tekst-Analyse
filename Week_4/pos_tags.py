9aa6656a760ce1cc1b395393f79c67580896c57b
root <root@DESKTOP-V8IFR35.localdomain>
2021-05-06 13:59:16 +0200

    exercise 1 almost complete

--- Week_4/pos_tags.py
+++ Week_4/pos_tags.py
@@ -12,14 +12,16 @@ def pos_tags(tokens):
 
 def main():
 	for root, dirs, files in os.walk("group3", topdown=False):
-	    for name in files:
-	        if name == "en.tok.off":
-	        	text = open(os.path.join(root, name)).read()
-	        	
-
-	        	#tokens = nltk.word_tokenize(text)
-	        	#pos_tagged_list = pos_tags(tokens)
-	        	#print(pos_tagged_list)
+		for name in files:
+			if name == "en.tok.off":
+				text = open(os.path.join(root, name)).readlines()
+				for line in text:
+					line = line.rstrip()
+					word = line.split()[3]
+					token = nltk.word_tokenize(word)
+					pos_tagged = pos_tags(token)
+					line = line + " " + pos_tagged[0][1]
+					print(line)
 
 if __name__ == '__main__':
     main()
