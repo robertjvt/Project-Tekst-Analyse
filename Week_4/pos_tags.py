f578512b41d491f69e769c20fd9ff4646aa41e2e
Robert <robertvantimmeren@gmail.com>
2021-05-06 14:22:26 +0200

    Tags and writes to correct directory.

--- Week_4/pos_tags.py
+++ Week_4/pos_tags.py
@@ -14,12 +14,17 @@ def main():
 	for root, dirs, files in os.walk("group3", topdown=False):
 	    for name in files:
 	        if name == "en.tok.off":
-	        	text = open(os.path.join(root, name)).read()
-	        	
-
-	        	#tokens = nltk.word_tokenize(text)
-	        	#pos_tagged_list = pos_tags(tokens)
-	        	#print(pos_tagged_list)
+	        	text = open(os.path.join(root, name)).readlines()
+	        	f = open(os.path.join(root) + "/en.tok.off.pos", "w")
+	        	for line in text:
+                        	line = line.rstrip()
+                        	token = line.split()[3]
+                        	token = nltk.word_tokenize(token)
+                        	pos_tag = nltk.pos_tag(token)
+                        	output = line + ' ' + pos_tag[0][1] + '\n'
+                        	f.write(output)
+	        	f.close()
+                        	
 
 if __name__ == '__main__':
     main()
