4a66761cc472f47754f9a0e2940b39442ba5939f
root <root@DESKTOP-V8IFR35.localdomain>
2021-05-06 13:32:52 +0200

    finding files

--- Week_4/pos_tags.py
+++ Week_4/pos_tags.py
@@ -2,9 +2,13 @@
 # Names: Robert van Timmeren, Kylian de rooij, Remi Thüss
 
 import nltk
+import os
 
 def main():
-	
+	for root, dirs, files in os.walk("group3", topdown=False):
+	    for name in files:
+	        if name == "en.tok.off":
+	            
 
 if __name__ == '__main__':
     main()
