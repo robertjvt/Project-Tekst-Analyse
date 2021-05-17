433d36954df2e95c24c0ddff64beef90adada3b9
Kylian de Rooij <kylianderooij@gmail.com>
2021-05-17 16:56:43 +0200

    Added: measures.py

--- Week_4/measures.py
+++ Week_4/measures.py
@@ -1,58 +1,118 @@
-from collections import Counter
-from nltk.metrics import ConfusionMatrix
+# File name: measures.py
+# Calling program: 'python3 measures.py [filename1] [filename2]
+# These filenames are the names that all the files should have in each folder.
+# Example: 'python3 test2.py en.tok.off.pos.kylian en.tok.off.pos.robert'
+# Description: This program will take annotated files and print two different
+# classification reports along with a confusion matrix.
+# Authors: Kylian de Rooij, Remi Thüss, Robert van Timmeren
+# Date: 17-05-2021
+
+
+import sys
 import os
+from nltk.metrics import ConfusionMatrix
+from sklearn.metrics import classification_report
 
-ref = 'DET NN VB DET JJ NN NN IN DET NN'.split()
-tagged = 'DET VB VB DET NN NN NN IN DET NN'.split()
-cm = ConfusionMatrix(ref, tagged)
 
-print(cm)
+def clean_labels(l1, l2):
+    '''
+    This function clears the labels from instances where both lists have
+    'None' at the same index.
+    '''
+    cl1, cl2 = [], []
+    for i in range(len(l1)):
+        if l1[i] != 'None' or l2[i] != 'None':
+            cl1.append(l1[i])
+            cl2.append(l2[i])
+    return cl1, cl2
 
-labels = set('DET NN VB IN JJ'.split())
 
-true_positives = Counter()
-false_negatives = Counter()
-false_positives = Counter()
+def clean_bool_labels(bl1, bl2):
+    '''
+    This function clears the boolean labels from instances where both lists
+    have 'False' at the same index.
+    '''
+    cbl1, cbl2 = [], []
+    for i in range(len(bl1)):
+        if bl1[i] == 'True' or bl2[i] == 'True':
+            cbl1.append(bl1[i])
+            cbl2.append(bl2[i])
+    return cbl1, cbl2
 
-for root, dirs, files in os.walk("group3", topdown=False):
-      for name in files:
-          if name == "en.tok.off_Remi.txt":
-            text = open(os.path.join(root, name)).readlines()
-            tokens = []
 
-            annotated = []
-            i = -1
-            for line in text:
-              i += 1
-              line = line.rstrip()
-              line = line.split()
-              if len(line) == 7:
-                annotated.append((i, line[5]))
-            print(annotated)
-          elif name == "en.tok.off.pos.kylian":
-            print("Reading Kylian file")
-          elif name == "en.tok.off.pos.robert":
-            print("Reading robert file")
-
-for i in labels:
-    for j in labels:
-        if i == j:
-            true_positives[i] += cm[i, j]
+def give_label(root, name, anno, labels, bool_labels):
+    '''
+    This function parses through a file and gives it labels where appropriate.
+    '''
+    with open(os.path.join(root, name)) as f:
+        text = f.readlines()
+    for line in text:
+        linelist = line.split()
+        # If the linelist is longer than 5, there is an annotation
+        if len(linelist) > 5:
+            anno.append([linelist[3], linelist[5]])
+            labels.append(linelist[5])
+            bool_labels.append('True')
+        # If the linelist is not longer than 5, there is no annotation
         else:
-            false_negatives[i] += cm[i, j]
-            false_positives[j] += cm[i, j]
+            anno.append([linelist[3], 'None'])
+            labels.append('None')
+            bool_labels.append('False')
+    return anno, labels, bool_labels
 
-print("TP:", sum(true_positives.values()), true_positives)
-print("FN:", sum(false_negatives.values()), false_negatives)
-print("FP:", sum(false_positives.values()), false_positives)
 
-for i in sorted(labels):
-    if true_positives[i] == 0:
-        fscore = 0
-    else:
-        precision = true_positives[i] / float(true_positives[i] +
-                                              false_positives[i])
-        recall = true_positives[i] / float(true_positives[i] +
-                                           false_negatives[i])
-        fscore = 2 * (precision * recall) / float(precision + recall)
-    print(i, fscore)
+def word_labels(file1, file2):
+    '''
+    This function uses other functions to parse the annotated files and
+    create lists with the words and their corresponding labels.
+    '''
+    # anno is usually not used, but if was found useful when adjudicating
+    # anno includes word and its assigned label
+    anno1, anno2 = [], []
+    # labels only includes the label
+    labels1, labels2 = [], []
+    # bool_labels includes 'True' and 'False' depending on whether there is a
+    # label present
+    bool_labels1, bool_labels2 = [], []
+    
+    # This iterates through the directories and files
+    for root, dirs, files in os.walk("group3", topdown=False):
+        for name in files:
+            # If the name is the same as the first file input into the
+            # program
+            if name == file1:
+                anno1, labels1, bool_labels1 = give_label(root, name, anno1, labels1, bool_labels1)
+            # If the name is the same as the second file input into the
+            # program
+            elif name == file2:
+                anno2, labels2, bool_labels2 = give_label(root, name, anno2, labels2, bool_labels2)
+    return labels1, labels2, bool_labels1, bool_labels2
+
+
+def print_agreement(cl1, cl2, cbl1, cbl2):
+    '''
+    This function prints two classification reports and a confusion matrix
+    '''
+    print('Agreement on finding entities:')
+    print(classification_report(cbl1, cbl2, digits=3))
+    print('Agreement on classifying entities:')
+    print(classification_report(cl1, cl2, digits=3))
+    print('Confusion matrix:')
+    print(ConfusionMatrix(cl1, cl2))
+
+
+def main(file1, file2):
+    labels1, labels2, bool_labels1, bool_labels2, = word_labels(file1, file2)
+    cl1, cl2 = clean_labels(labels1, labels2)
+    cbl1, cbl2 = clean_bool_labels(bool_labels1, bool_labels2)
+    print_agreement(cl1, cl2, cbl1, cbl2)
+
+
+if len(sys.argv) != 3:
+    print('Error:')
+    print('This program takes two positional arguments, {0} were given'.format(len(sys.argv)-1))
+    print('Please use the following format:')
+    print('python3 test2.py [filename 1] [filename 2]')
+    exit()
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
