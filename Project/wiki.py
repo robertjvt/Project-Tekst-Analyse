c5826748138ace47a1f1871701f525240d1a4a84
Kylian de Rooij <kylianderooij@gmail.com>
2021-06-03 10:32:56 +0200

    Fixed wikipedia.exception.DisambiguationError

--- Project/wiki.py
+++ Project/wiki.py
@@ -1,20 +1,24 @@
 import sys
 import os
-import wikipedia as wiki
+import wikipedia
 
 
 def wiki_finder(NE):
-    wiki_search = wiki.search(NE)
-    page = wiki.page(wiki_search[0], auto_suggest = True)
-    
+    wiki_search = wikipedia.search(NE)
+    try:
+        page = wikipedia.page(wiki_search[0])
+    except wikipedia.exceptions.DisambiguationError as e:
+        page = wikipedia.page(e.options[0])
+
     return page.url
 
 
-
 def main():
-    NE_list = ['Obama', 'Facebook', 'Mexico']
+    NE_list = ['Obama', 'Facebook', 'Mexico', 'USA', 'KFC']
     for NE in NE_list:
         url = wiki_finder(NE)
+        print(url)
+
 
 if __name__ == '__main__':
     main()
