9c9ae86da02a95a6a83f737b371bf7e3e9e569dd
Kylian de Rooij <kylianderooij@gmail.com>
2021-06-05 17:07:26 +0200

    -Wiki function now checks whether link is actually available -entity features function now checks whether wiki function returned a link -added a clean list function to remove all instances of None in a list

--- Project/features2.py
+++ Project/features2.py
@@ -22,22 +22,22 @@ def NE_features(data, i):
             'previous_word': data[i-1][3],
             'previous_POS': data[i-1][4],
         })
-        
+
         if len(data[i-1]) == 6:
             features.update({
                 'previous_NE': data[i-1][5]
-        })
-        
+            })
+
     if i < len(data) - 1:
         features.update({
             'next_word': data[i+1][3],
             'next_POS': data[i+1][4],
         })
-        
+
         if len(data[i+1]) == 6:
             features.update({
                 'next_NE': data[i+1][5]
-        })
+            })
 
     return features
 
@@ -50,6 +50,11 @@ def list_features(data):
 
 
 def group_NE(DF):
+    '''
+    This function groups named entities by looking at the next words and their
+    tags. It uses other functions to create a list of dictionaries
+    (each dictionary containing features about the named entity).
+    '''
     NE_length = 0
     NE_list = []
     for i in range(len(DF)):
@@ -71,30 +76,55 @@ def group_NE(DF):
                     NE_length = 0
         except KeyError as e:
             NE_length = 0
-    return NE_list
+    return clean_list(NE_list)
 
 
 def entity_features(DF, i, NE_length):
+    '''
+    This function creates a list of dictionaries for each named entity.
+    It also checks whether there is a Wikipedia link, if there is not, the NE
+    is ignored.
+    '''
     NNP = []
     for j in range(NE_length):
         NNP.append(DF[(i-NE_length+1)+j]['word'])
     entity = ' '.join(NNP)
-    features = {
-        'entity': entity,
-        'NE': DF[i]['NE'],
-        'wiki': wiki_finder(entity),
-        'lower_index': i - NE_length + 1,
-        'upper_index': i
-        }
-    return features
+    url = wiki_finder(entity)
+    if url is not None:
+        features = {
+            'entity': entity,
+            'NE': DF[i]['NE'],
+            'wiki': url,
+            'lower_index': i - NE_length + 1,
+            'upper_index': i
+            }
+        return features
 
 
-def wiki_finder(NE):    
+def clean_list(NE_list):
+    '''
+    This function removes any instances of None in a list
+    '''
+    for i in range(len(NE_list)):
+        if NE_list[i] is None:
+            del NE_list[i]
+    return NE_list
+
+
+def wiki_finder(NE):
+    '''
+    This function takes a named entity, checks if there is a Wikipedia page
+    for it and if so, returns the url.
+    '''
     wikipedia = MediaWiki()
     wiki_search = wikipedia.search(NE)
-    page = wikipedia.page(wiki_search[0])
-
-    return page.url
+    try:
+        if wiki_search[0]:
+            page = wikipedia.page(wiki_search[0])
+            return page.url
+    except IndexError as e:
+        page = None
+        return page
 
 
 def main():
