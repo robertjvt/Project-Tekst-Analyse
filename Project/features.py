cd8e9c12de55b5dd26552bbe808c79ecb94967d4
Robert <robertvantimmeren@gmail.com>
2021-06-05 18:12:22 +0200

    New features file.

--- Project/features.py
+++ Project/features.py
@@ -1,6 +1,9 @@
 import sys
 import os
-import wikipedia
+
+from mediawiki import MediaWiki
+import nltk
+from nltk.corpus import wordnet
 
 
 def NE_features(data, i):
@@ -10,6 +13,7 @@ def NE_features(data, i):
     features = {
         'word': data[i][3],
         'POS': data[i][4],
+        'index': i
     }
     if len(data[i]) == 6:
         features.update({
@@ -48,27 +52,97 @@ def list_features(data):
     return [NE_features(data, i) for i in range(len(data))]
 
 
-def wiki_finder(data_features):    
-    wiki_search = wikipedia.search(data[1])
-    try:
-        page = wikipedia.page(wiki_search[0])
-    except wikipedia.exceptions.DisambiguationError as e:
-        page = wikipedia.page(e.options[0])
+def group_NE(DF):
+    NE_length = 0
+    NE_list = []
+    for i in range(len(DF)):
+        try:
+            if DF[i]['NE']:
+                NE_length += 1
+                try:
+                    if DF[i+1]['NE']:
+                        continue
+                except IndexError as e:
+                    NNP = []
+                    for j in range(NE_length):
+                        NNP.append(DF[(i-NE_length+1)+j]['word'])
+                    entity = ' '.join(NNP)
+                    NE_list.append({
+                        'entity': entity,
+                        'NE': DF[i]['NE'],
+                        'wiki': wiki_finder(entity),
+                        'lower_index': i - NE_length + 1,
+                        'upper_index': i
+                    })
+                except KeyError as e:
+                    NNP = []
+                    for j in range(NE_length):
+                        NNP.append(DF[(i-NE_length+1)+j]['word'])
+                    entity = ' '.join(NNP)
+                    NE_list.append({
+                        'entity': entity,
+                        'NE': DF[i]['NE'],
+                        'wiki': wiki_finder(entity),
+                        'lower_index': i - NE_length + 1,
+                        'upper_index': i
+                    })
+        except KeyError as e:
+            NE_length = 0
+    return NE_list
 
-    print(page.url)
+
+def wiki_finder(NE):    
+    wikipedia = MediaWiki()
+    wiki_search = wikipedia.search(NE)
+    page = wikipedia.page(wiki_search[0])
+
     return page.url
 
+
+def noun_finder(summary):
+    sentence_tokens = nltk.sent_tokenize(summary)
+    word_tokens = []
+    noun_list = []
+    for sentence in sentence_tokens:
+        word_tokens.append(nltk.word_tokenize(sentence))
+    for sentence in word_tokens:
+        pos_tags = nltk.pos_tag(sentence)
+        for tagged_word in pos_tags:
+            if tagged_word[1][:2] == "NN":
+                noun_list.append([tagged_word[0], sentence])
+    return noun_list
+
+
+def synset_finder(noun_list):
+    synset_list = []
+    for noun in noun_list:
+        synsets = wordnet.synsets(noun[0], 'n')
+        synset_list.append(synsets)
+            
+    
+
+def find_wikipedia(NE):
+    wikipedia = MediaWiki()
+    possible_pages = wikipedia.search(NE)
+    for item in possible_pages:
+        page = wikipedia.page(item)
+        summary = page.summary
+        noun_list = noun_finder(summary)
+        print(noun_list)
+        
 
 def main():
     with open('test.output') as file:
         data = []
         for line in file:
             line = line.rstrip().split()
-            data.append(line)
-
+            data.append(line)        
     data_features = list_features(data)
-    print(data_features)
-
+    NE_list = group_NE(data_features)
+    
+    for dictionary in NE_list:
+        find_wikipedia(dictionary['entity'])
+        
 
 if __name__ == '__main__':
     main()
